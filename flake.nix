a5da314582c81b32dbb8e0e13584acadd5e60bf5
Kirill Elagin <kirelagin@gmail.com>
2022-04-26 10:19:36 +0200

    fixup! mail: Setup home-manager mail fetching

--- flake.nix
+++ flake.nix
@@ -19,8 +19,7 @@
       flake = false;
     };
     home-manager = {
-      #url = "github:nix-community/home-manager";
-      url = "/home/kirelagin/proj/home-manager";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
