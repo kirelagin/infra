471ce93f71b56c1ae23557e780cc8b84c1725959
Kirill Elagin <kirelagin@gmail.com>
2024-03-02 14:47:51 -0500

    elagin.me: Only keep 4 BuddyNS secondaries

--- modules/apps/dns/elagin.me.nix
+++ modules/apps/dns/elagin.me.nix
@@ -31,18 +31,19 @@ let
       "2a10:1fc0:d::ae75:f39a" "2a01:a500:2766::5c3f:d10b" "2602:fafd:902:51::a" "2406:d500:2::de4f:f105" "2604:180:1:92a::3"
       "2606:fc40:4003:26::a" "2a10:1fc0:1::e313:41be" "2604:180:2:4cf::3" "2a01:4f8:1c0c:8115::3" "2001:19f0:6400:8642::3"
     ];
+    notify = [ "2a01:4f8:1c0c:8115::3" "2604:180:2:4cf::3" "2606:fc40:4003:26::a" "2602:fafd:902:51::a" ];
     ns = [
-      "uz53c7fwlc89h7jrbxcsnxfwjw8k6jtg56l4yvhm6p2xf496c0xl40.free.ns.buddyns.com."  # b
-      "uz5x36jqv06q5yulzwcblfzcrk1b479xdttdm1nrgfglzs57bmctl8.free.ns.buddyns.com."  # c
-      "uz588h0rhwuu3cc03gm9uckw0w42cqr459wn1nxrbzhym2wd81zydb.free.ns.buddyns.com."  # d
-      "uz5c15kc3lkws2mtwp7l8g9f33yffvvt96y54tlmn41zjy0043purm.free.ns.buddyns.com."  # e
-      "uz5154v9zl2nswf05td8yzgtd0jl6mvvjp98ut07ln0ydp2bqh1skn.free.ns.buddyns.com."  # f
-      "uz5dkwpjfvfwb9rh1qj93mtup0gw65s6j7vqqumch0r9gzlu8qxx39.free.ns.buddyns.com."  # g
-      "uz5w6sb91zt99b73bznfkvtd0j1snxby06gg4hr0p8uum27n0hf6cd.free.ns.buddyns.com."  # h
-      "uz5qfm8n244kn4qz8mh437w9kzvpudduwyldp5361v9n0vh8sx5ucu.free.ns.buddyns.com."  # i
-      "uz56xw8h7fw656bpfv84pctjbl9rbzbqrw4rpzdhtvzyltpjdmx0zq.free.ns.buddyns.com."  # j
-      "uz5x6wcwzfbjs8fkmkuchydn9339lf7xbxdmnp038cmyjlgg9sprr2.free.ns.buddyns.com."  # k
-      "uz52u1wtmumlrx5fwu6nmv22ntcddxcjjw41z8sfd6ur9n7797lrv9.free.ns.buddyns.com."  # l
+      "uz5x36jqv06q5yulzwcblfzcrk1b479xdttdm1nrgfglzs57bmctl8.free.ns.buddyns.com."  # c (Germany)
+      "uz588h0rhwuu3cc03gm9uckw0w42cqr459wn1nxrbzhym2wd81zydb.free.ns.buddyns.com."  # d (New York)
+      "uz5154v9zl2nswf05td8yzgtd0jl6mvvjp98ut07ln0ydp2bqh1skn.free.ns.buddyns.com."  # f (Singapore)
+      "uz5qfm8n244kn4qz8mh437w9kzvpudduwyldp5361v9n0vh8sx5ucu.free.ns.buddyns.com."  # i (California)
+      # "uz53c7fwlc89h7jrbxcsnxfwjw8k6jtg56l4yvhm6p2xf496c0xl40.free.ns.buddyns.com."  # b (Texas)
+      # "uz5c15kc3lkws2mtwp7l8g9f33yffvvt96y54tlmn41zjy0043purm.free.ns.buddyns.com."  # e (Estonia)
+      # "uz5dkwpjfvfwb9rh1qj93mtup0gw65s6j7vqqumch0r9gzlu8qxx39.free.ns.buddyns.com."  # g (Seattle)
+      # "uz5w6sb91zt99b73bznfkvtd0j1snxby06gg4hr0p8uum27n0hf6cd.free.ns.buddyns.com."  # h (Adelaide)
+      # "uz56xw8h7fw656bpfv84pctjbl9rbzbqrw4rpzdhtvzyltpjdmx0zq.free.ns.buddyns.com."  # j (UK)
+      # "uz5x6wcwzfbjs8fkmkuchydn9339lf7xbxdmnp038cmyjlgg9sprr2.free.ns.buddyns.com."  # k (Israel)
+      # "uz52u1wtmumlrx5fwu6nmv22ntcddxcjjw41z8sfd6ur9n7797lrv9.free.ns.buddyns.com."  # l (Japan)
     ];
   };
 
@@ -50,7 +51,7 @@ let
     SOA = {
       nameServer = "ns1";
       adminEmail = "kirelagin@gmail.com";
-      serial = 2023022301;
+      serial = 2024030200;
     };
 
     NS = lib.flatten [
@@ -155,7 +156,7 @@ in {
       {
         "elagin.me" = {
           provideXFR = map nokey (lib.flatten [ trifle.xfr afraid.xfr onecom.xfr buddyns.ips ]);
-          notify = map nokey (lib.flatten [ trifle.notify afraid.notify onecom.notify buddyns.ips ]);
+          notify = map nokey (lib.flatten [ trifle.notify afraid.notify onecom.notify buddyns.notify ]);
           data = flakes.dns.lib.toString "elagin.me" elagin-me-zone;
         };
         "kirelag.in" = {
