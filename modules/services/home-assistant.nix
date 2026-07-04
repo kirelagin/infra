e53e8b8f6188c9c00c6450ddbdeea683874ab0b3
Kirill Elagin <kirelagin@gmail.com>
2026-07-04 13:15:57 +0200

    home-assistant: Bump the version

--- modules/services/home-assistant.nix
+++ modules/services/home-assistant.nix
@@ -8,16 +8,16 @@ let
   images = {
     ha = {
       imageName = "homeassistant/home-assistant";
-      imageDigest = "sha256:c71e7a3780d917b8217c17515263d1461ec2bff192660d7dd30cd8889519ebfe";
-      sha256 = "sha256-OkkHRGMlT47kw8L+/CFxeeZa6qYl0X1EvZgcON5RMuI=";
+      imageDigest = "sha256:f73512ba4fe06bb4d57636fe3578d0820cdec46f81e8f837ab59e451662ff3cb";
+      sha256 = "sha256-39RzMAimG2Wt6a+jTBcJOkWmZvyXi3GBrt/uKm4gu/Q=";
     };
-    zwave-js-ui = {
-      imageName = "zwavejs/zwave-js-ui";
-      imageDigest = "sha256:c43f88e2e395bb4b37c9e87e64998b751b1e1908318177623cebe08867e1eb42";
-      sha256 = "";
-    };
+    #zwave-js-ui = {
+    #  imageName = "zwavejs/zwave-js-ui";
+    #  imageDigest = "";
+    #  sha256 = "";
+    #};
   };
-  zwave_dev = "/dev/serial/by-id/usb-Zooz_800_Z-Wave_stick_533D004242-if00";
+  #zwave_dev = "/dev/serial/by-id/usb-Zooz_800_Z-Wave_stick_533D004242-if00";
   zigbee_dev = "/dev/serial/by-id/usb-Itead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_V2_d017f77b9438ef119d823e7af3d9b1e5-if00-port0";
 
 in
